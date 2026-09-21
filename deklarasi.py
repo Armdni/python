8a131e4cf47d01bcf1449d737b80567c90d9efca
Armdni <auliaramadhani20200@gmail.com>
2026-09-21 21:22:45 +0700

    Add files via upload

---
+++ deklarasi.py
@@ -0,0 +1,8 @@
+nama = "Aulia"
+hobi = "Menggambar"
+umur = "19"
+perempuan = True
+
+print("=== biodata ===")
+print("nama: %s" % (nama))
+print("nama: %s, umur: %d, perempuan: %r" (nama, umur, perempuan))
