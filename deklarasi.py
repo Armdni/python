4c6d32904696cf7eabb5b412e49dd12ade4976bd
Armdni <auliaramadhani20200@gmail.com>
2026-09-21 21:31:58 +0700

    Update deklarasi.py

--- deklarasi.py
+++ deklarasi.py
@@ -1,8 +1,8 @@
 nama = "Aulia"
 hobi = "Menggambar"
-umur = "19"
+umur = 19
 perempuan = True
 
 print("=== biodata ===")
 print("nama: %s" % (nama))
-print("nama: %s, umur: %d, perempuan: %r" (nama, umur, perempuan))
+print("hobi: %s, umur: %d, perempuan: %r" % (hobi, umur, perempuan))
